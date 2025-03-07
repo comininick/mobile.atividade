b6db22883b5aaeaabeb9ade6e06d822acb9466f7
comininick <160253618+comininick@users.noreply.github.com>
2025-03-07 07:56:17 -0300

    Verificar se uma String é vazia.

---
+++ script.js
@@ -0,0 +1,13 @@
+var coisa = String( prompt("Digite alguma coisa") );
+var outra = String( prompt("Digite outra coisa") );
+
+if (coisa == outra) {
+
+alert("String vazia!");
+
+} else {
+
+alert("String não vazia!");
+
+
+}
